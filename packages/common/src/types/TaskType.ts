ebd9cc4cb0bb014831c3b313bb0c6c4636eb705b
James <5511220+Zamiell@users.noreply.github.com>
2022-08-05 02:18:35 -0400

    fix: lint

--- packages/common/src/types/TaskType.ts
+++ packages/common/src/types/TaskType.ts
@@ -1,5 +1,5 @@
-// We use a string enum instead of an integer enum since we need to use TaskType for the keys
-// TaskList, and converting integers to strings gets messy
+// We use a string enum instead of an integer enum since we need to use `TaskType` for the keys of
+// `TaskList`, and converting integers to strings gets messy.
 export enum TaskType {
   SHORT = "short",
   LONG = "long",
