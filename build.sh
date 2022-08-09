4513937ca5d68c508b3bb79d3a5eb4e9503560ec
James <5511220+Zamiell@users.noreply.github.com>
2022-08-09 14:16:45 -0400

    fix: linter

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ set -e # Exit on any errors
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-"$DIR/packages/mod/build.sh"
+"$DIR/packages/common/build.sh"
 "$DIR/packages/server/build.sh"
+"$DIR/packages/mod/build.sh"
