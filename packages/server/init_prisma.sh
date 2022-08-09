ccf84c460ad95fd46e8e44465e6c28759c2ce1a0
James <5511220+Zamiell@users.noreply.github.com>
2022-08-09 14:14:17 -0400

    fix: ci

---
+++ packages/server/init_prisma.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e # Exit on any errors
+
+# Get the directory of this script
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd "$DIR"
+
+# This must be run in CI in order for the TypeScript to properly compile.
+npx prisma generate
