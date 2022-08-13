964e835948716464e82bb381eed51f2bb5e40cb6
James <5511220+Zamiell@users.noreply.github.com>
2022-08-12 23:36:36 -0400

    chore: adjust dev constants

--- packages/server/src/constants.ts
+++ packages/server/src/constants.ts
@@ -7,6 +7,6 @@ export const DEFAULT_NUM_LONG_TASKS = 2;
 export const DEFAULT_NUM_COMMON_TASKS = 0;
 
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-export const DEFAULT_MEETING_PRE_VOTE_SECONDS = IS_DEV ? 1 : 15;
+export const DEFAULT_MEETING_PRE_VOTE_SECONDS = IS_DEV ? 15 : 15;
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-export const DEFAULT_MEETING_VOTE_SECONDS = IS_DEV ? 5 : 150;
+export const DEFAULT_MEETING_VOTE_SECONDS = IS_DEV ? 150 : 150;
